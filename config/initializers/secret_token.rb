a134d63472041a22d38d5a205a7df3b72df297a4
Brad Potterbaum <bpotterbaum@gmail.com>
2013-08-20 07:09:43 -0500

    init build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nocms::Application.config.secret_key_base = 'b371d8bb9f7f40fd949568e49b87c5662eacb790a3b96a85355f38dd2f5ab5c5d60eb4ff64f42d35d830c3c5c1157fffb51f71729bbf073e65e4ebf27ec28e80'
